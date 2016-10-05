273ed0c1960da389ba0310bd5a81a67d770c7381
frasaleksander <fongreecss@gmail.com>
2016-10-05 09:04:47 +0200

    update

---
+++ app/app.js
@@ -0,0 +1,18 @@
+var jQuery = window.jQuery = window.$ = require("jquery");
+window.Mustache = require("mustache");
+
+
+function renderTemplate(templateFile, jsonFile, renderElementSelector) {
+	// var template = $(templateSelector).html();
+	var rendered = null;
+	var jsonReq = $.getJSON(jsonFile);
+	var tempReq = $.get(templateFile);
+	$.when(tempReq, jsonReq).done(function(data1, data2){
+		var render = Mustache.render(data1[0], data2[0]);
+		$(renderElementSelector).html(render);
+	});
+}
+
+(function ($) {
+	renderTemplate('data/works.mst', 'data/works.json', '#work-collection');
+})(window.jQuery);
