f2847620576523d027901e8875b122375aac6c1e
frasaleksander <fongreecss@gmail.com>
2017-02-19 11:47:30 +0100

    update

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
+
+(function() {
+    var leftMenuButton = $('.left-menu-button');
+    var body = $('body');
+
+    leftMenuButton.on('click', function() {
+        if (body.attr('data-left-menu-is-active') == "true") {
+            body.attr('data-left-menu-is-active', "false");
+            leftMenuButton.attr('data-left-menu-is-active', "false");
+        } else {
+            body.attr('data-left-menu-is-active', "true");
+            leftMenuButton.attr('data-left-menu-is-active', "true");
+        }
+    });
+})();
--- app/build.js
+++ app/build.js
@@ -1,5 +1,20 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
+
+(function() {
+    var leftMenuButton = $('.left-menu-button');
+    var body = $('body');
+
+    leftMenuButton.on('click', function() {
+        if (body.attr('data-left-menu-is-active') == "true") {
+            body.attr('data-left-menu-is-active', "false");
+            leftMenuButton.attr('data-left-menu-is-active', "false");
+        } else {
+            body.attr('data-left-menu-is-active', "true");
+            leftMenuButton.attr('data-left-menu-is-active', "true");
+        }
+    });
+})();
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.1.1
